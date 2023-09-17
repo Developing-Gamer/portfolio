43d5f62867f666e6ec33d0b85e881707ba41f7b6
Armaan Jain <armaan_jain@Armaans-MacBook-Pro-2.local>
2023-09-17 18:55:17 -0400

    Scroll Anim Added

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(function () {
+    // init feather icons
+    feather.replace();
+
+    //page scroll
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 20
+        }, 1000);
+        event.preventDefault();
+    });
+});
